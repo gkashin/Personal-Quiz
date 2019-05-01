eb465198260d57fc8a5286ec07a00d4c487dea7b
George <georgii.kashin@gmail.com>
2019-05-01 15:18:51 +0300

    Initial Commit

---
+++ Personal Quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Personal Quiz
+//
+//  Created by Георгий Кашин on 01/05/2019.
+//  Copyright © 2019 Georgii Kashin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
